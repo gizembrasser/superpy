66cfd7fef774ad249848deeb055ed0fee96ffb55
gizembrasser <gizembrasser101@gmail.com>
2024-01-01 11:44:45 +0100

    new: get_date command for CLI

---
+++ core/create_parser.py
@@ -0,0 +1,11 @@
+import argparse
+
+def create_parser():
+    parser = argparse.ArgumentParser(prog="python super.py", 
+                                     description="Superpy: Supermarket inventory management", 
+                                     epilog="Thanks for using Superpy!")
+    
+    subparsers = parser.add_subparsers(dest="command", title="commands", help="Choose a command")
+
+    # Get current date command
+    subparsers.add_parser("get_date", help="Get the current date")
