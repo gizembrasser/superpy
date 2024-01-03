d332a6fdb893e016430309dfbcbac921b7dabe9a
gizembrasser <gizembrasser101@gmail.com>
2024-01-03 17:31:08 +0100

    fix: indentation

--- services/sell.py
+++ services/sell.py
@@ -32,34 +32,30 @@ def sell_product(product_name, sell_price, count):
 
     # The filtered_inventory list should contain the found product(s) if it is in stock.
     if len(filtered_inventory) == 0:
-        return f"No {product_name} found in stock."
+        print(f"No {product_name} found in stock.")
     elif int(filtered_inventory[0]["count"]) < count:
-        answer = input(f"Not enough {product_name} in stock. Do you want to sell the remaining {filtered_inventory[0]["count"]} items? (y/n): ")
-        if answer.lower() == "y":
-            count = int(filtered_inventory[0]["count"])
-            sell_product(product_name, sell_price, count)
-        else:
-            return "Sale cancelled."
-        
-    # Assign the data to the right keys so it can be written to the correct headers in the CSV file.   
-    for item in filtered_inventory:
-        sold_data = {
-            "sold_id": item["bought_id"],
-            "product_name": item["product_name"],
-            "buy_date": item["buy_date"],
-            "buy_price": item["buy_price"],
-            "count": count,
-            "sell_date": sell_date,
-            "sell_price": sell_price
-        }
+        print(f"Not enough {product_name} in stock. Maximum quantity: {filtered_inventory[0]["count"]}")
+    
+    else:
+        # Assign the data to the right keys so it can be written to the correct headers in the CSV file.   
+        for item in filtered_inventory:
+            sold_data = {
+                "sold_id": item["bought_id"],
+                "product_name": item["product_name"],
+                "buy_date": item["buy_date"],
+                "buy_price": item["buy_price"],
+                "count": count,
+                "sell_date": sell_date,
+                "sell_price": sell_price
+            }
 
-    # Append the SOLD_FILE with the sold product.
-    with open(SOLD_FILE, mode="a", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(sold_data.values())
+        # Append the SOLD_FILE with the sold product.
+        with open(SOLD_FILE, mode="a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(sold_data.values())
     
                    
-sell_product("orange", 0.9, 300)
+"""sell_product("orange", 0.9, 200)"""
 
 
 
