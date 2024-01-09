b3dee751231b970fff9e436ec2ff83cae1a6cfcf
gizembrasser <gizembrasser101@gmail.com>
2024-01-09 11:04:54 +0100

    add: test_output_table()

--- utils/__tests__/test_output_table.py
+++ utils/__tests__/test_output_table.py
@@ -7,3 +7,16 @@ sys.path.append(grandparent_dir)
 from output_table import output_table
 
 
+def test_output_table(capsys):
+    output_table("revenue")
+
+    # Capture the output.
+    captured = capsys.readouterr()
+    actual_output = captured.out.strip() # Remove leading/trailing whitespaces.
+
+    expected_output = '     Revenue table:     \n┏━━━━━━━━━━━━┳━━━━━━━━━┓\n┃   period   ┃ revenue ┃\n┡━━━━━━━━━━━━╇━━━━━━━━━┩\n│ 2024-01-01 │  79.2   │\n│ 2024-01-06 │  22.5   │\n│ 2024-01-07 │  75.0   │\n└────────────┴─────────┘'
+    assert "error" not in captured.err # Ensure no error messages are printed
+
+
+
+
